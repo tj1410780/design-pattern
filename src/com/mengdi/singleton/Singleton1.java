b8c443fa87ba2f51e74843db1166a31f4022d41e
tj1410780 <qidizh@126.com>
2018-05-03 09:51:48 +0800

    Update Singleton1.java

--- src/com/mengdi/singleton/Singleton1.java
+++ src/com/mengdi/singleton/Singleton1.java
@@ -1,22 +1,22 @@
 package com.mengdi.singleton;
 /**
- * ����ģʽSingleton
+ * 单例模式Singleton
  * @author qidi
- * @date 2018��1��3�� @time ����1:49:48
- * Ӧ�ó�������Щ����ֻ��Ҫһ���͹��ˣ����̳߳أ����棬�Ի������Ӷ���
- * ���ã���֤����Ӧ�ó�����ĳ��ʵ������ֻ��һ��
- * ���ͣ�����ģʽ������ģʽ
+ * @date 2018年1月3日 @time 下午1:49:48
+ * 应用场景：有些对象只需要一个就够了，如线程池，缓存，对话框，日志对象
+ * 作用：保证整个应用程序中某个实例有且只有一个
+ * 类型：饿汉模式、懒汉模式
  */
 
 /*
- * ����ģʽ
+ * 饿汉模式
  */
 public class Singleton1 {
-	//1.�����췽��˽�л����������ⲿֱ�Ӵ�������
+	//1.将构造方法私有化，不允许外部直接创建对象
 	private Singleton1() {}
-	//2.�������Ψһʵ����ʹ��public static����
+	//2.创建类的唯一实例，使用public static修饰
 	private static Singleton1 instance = new Singleton1();
-	//3.�ṩһ�����ڻ�ȡʵ���ķ�����ʹ��public static����
+	//3.提供一个用于获取实例的方法，使用public static修饰
 	public static Singleton1 getInstance() {
 		return instance;
 	}
